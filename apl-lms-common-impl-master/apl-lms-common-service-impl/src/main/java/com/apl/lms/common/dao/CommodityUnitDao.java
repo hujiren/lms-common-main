b7f5ade0879377a1d1007a8e640b3f1ca7b11f06
apl0219 <1901080219@aplun.net>
2020-07-20 19:49:21 +0800

    apl-db

--- apl-lms-common-impl-master/apl-lms-common-service-impl/src/main/java/com/apl/lms/common/dao/CommodityUnitDao.java
+++ apl-lms-common-impl-master/apl-lms-common-service-impl/src/main/java/com/apl/lms/common/dao/CommodityUnitDao.java
@@ -4,17 +4,16 @@ import com.alibaba.druid.pool.DruidDataSource;
 import com.apl.cache.AplCacheUtil;
 import com.apl.db.datasource.DataSourceConfig;
 import com.apl.db.datasource.DynamicDataSource;
-import com.apl.db.utils.DBUtil;
-import com.apl.lib.cachebase.BaseCacheUtil;
+import com.apl.db.orm.AplDBInfo;
+import com.apl.db.orm.AplDBPersistent;
+import com.apl.db.orm.AplDBQuery;
 import com.apl.lib.pojo.dto.PageDto;
 import com.apl.lib.security.SecurityUser;
 import com.apl.lib.utils.CommonContextHolder;
 import com.apl.lms.common.query.manage.dto.CommodityUnitDto;
 import com.apl.lms.common.query.manage.dto.CommodityUnitKeyDto;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,13 +22,10 @@ import java.util.Map;
 public class CommodityUnitDao {
 
     @Autowired
-    DBUtil dbUtil;
-
-    @Autowired
     AplCacheUtil aplCacheUtil;
 
 
-    public DBUtil.DBInfo createDBinfo(){
+    public AplDBInfo connectDb(){
         // 创建数据库连接信息
         SecurityUser securityUser = CommonContextHolder.getSecurityUser();
 
@@ -37,51 +33,49 @@ public class CommodityUnitDao {
                 DataSourceConfig.sysProduct,
                 "wms_stocks_history",
                 aplCacheUtil);
-        DBUtil.DBInfo dbInfo = dbUtil.connect(druidDataSource);
+        AplDBInfo dbInfo = new AplDBInfo(druidDataSource);
         dbInfo.setTenantValue(securityUser.getInnerOrgId());
-        DBUtil.DBInfo.tenantIdName = "inner_org_id";
-
-        dbInfo.dbUtil = dbUtil;
+        AplDBInfo.tenantIdName = "inner_org_id";
 
         return dbInfo;
     }
 
     // 添加
-    public Long add(DBUtil.DBInfo dbInfo, CommodityUnitDto commodityUnit) throws Exception {
+    public Long add(AplDBInfo dbInfo, CommodityUnitDto commodityUnit) throws Exception {
 
-        return dbUtil.insertReturnId(dbInfo, commodityUnit, "commodity_unit");
+        return AplDBPersistent.insertReturnId(dbInfo, commodityUnit, "commodity_unit");
     }
 
 
     // 修改
-    public Integer upd(DBUtil.DBInfo dbInfo, CommodityUnitDto commodityUnit) throws Exception {
+    public Integer upd(AplDBInfo dbInfo, CommodityUnitDto commodityUnit) throws Exception {
 
-        return  dbUtil.updateById(dbInfo, commodityUnit, "commodity_unit");
+        return  AplDBPersistent.updateById(dbInfo, commodityUnit, "commodity_unit");
     }
 
 
     // 删除
-    public Integer del(DBUtil.DBInfo dbInfo, Long id) throws Exception {
+    public Integer del(AplDBInfo dbInfo, Long id) throws Exception {
 
-        return dbUtil.delById(dbInfo, "commodity_unit", id);
+        return AplDBPersistent.delById(dbInfo, "commodity_unit", id);
     }
 
 
     // 查询单个
-    public CommodityUnitDto selectById(DBUtil.DBInfo dbInfo, Long id){
+    public CommodityUnitDto selectById(AplDBInfo dbInfo, Long id){
 
         String sql = "select id,unit_code,unit_name FROM commodity_unit where id="+id.toString();
-        CommodityUnitDto commodityUnitInfoVo = dbUtil.queryObj(dbInfo, sql, id, CommodityUnitDto.class);
+        CommodityUnitDto commodityUnitInfoVo = AplDBQuery.queryObj(dbInfo, sql, id, CommodityUnitDto.class);
 
         return commodityUnitInfoVo;
     }
 
 
     // 分页查询
-    public List<CommodityUnitDto> getList(DBUtil.DBInfo dbInfo, PageDto pageDto, CommodityUnitKeyDto keyDto){
+    public List<CommodityUnitDto> getList(AplDBInfo dbInfo, PageDto pageDto, CommodityUnitKeyDto keyDto){
 
         String sql = " SELECT  id, unit_code, unit_name FROM commodity_unit";
-        List<CommodityUnitDto>  list = dbUtil.queryPage(
+        List<CommodityUnitDto>  list = AplDBQuery.queryPage(
                 dbInfo,
                 sql,
                 keyDto,
@@ -95,7 +89,7 @@ public class CommodityUnitDao {
 
 
     // 检测是否存在
-    public List<CommodityUnitDto> exists(DBUtil.DBInfo dbInfo, Long id, String unitCode, String unitName){
+    public List<CommodityUnitDto> exists(AplDBInfo dbInfo, Long id, String unitCode, String unitName){
 
         Map<String, Object> paramMap = new HashMap<>();
         paramMap.put("id", id);
@@ -108,7 +102,7 @@ public class CommodityUnitDao {
         if(id!=null && id>0)
             sql.append(" and id<>"+id.toString());
 
-        List<CommodityUnitDto>   list = dbUtil.queryList(
+        List<CommodityUnitDto>   list = AplDBQuery.queryList(
                 dbInfo,
                 sql.toString(),
                 paramMap,
