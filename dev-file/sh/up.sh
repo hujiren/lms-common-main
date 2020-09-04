96de4a3464848897c28c8001451e4ed100447790
arranchen <arran@aplun.net>
2020-09-04 15:32:35 +0800

    update .gitignore

--- dev-file/sh/up.sh
+++ dev-file/sh/up.sh
@@ -7,7 +7,7 @@ MYPATH="/Volumes/data/java/apl-lms/apl-lms-common-java"
 
 
 # 公共模块管理
- scp  $MYPATH/apl-lms-common-impl-master/apl-lms-common-manage-app/target/apl-lms-common-manage-app-1.0.0.jar  root@192.168.1.185:/usr/local/java/basic/
+# scp  $MYPATH/apl-lms-common-impl-master/apl-lms-common-manage-app/target/apl-lms-common-manage-app-1.0.0.jar  root@192.168.1.185:/usr/local/java/basic/
 
 # 公共模块查询
-# scp  $MYPATH/apl-lms-common-impl-master/apl-lms-common-query-app/target/apl-lms-common-query-app-1.0.0.jar root@192.168.1.185:/usr/local/java/basic/
+ scp  $MYPATH/apl-lms-common-impl-master/apl-lms-common-query-app/target/apl-lms-common-query-app-1.0.0.jar root@192.168.1.185:/usr/local/java/basic/
